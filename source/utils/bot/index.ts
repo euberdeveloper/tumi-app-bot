b5c5b93927076c568e5b28d2bc6b2f649f6344f7
euberdeveloper <euberdeveloper@gmail.com>
2021-10-18 15:03:09 +0200

    feat(bot): add bold text
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/utils/bot/index.ts
+++ source/utils/bot/index.ts
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf';
+import * as dateAndTime from 'date-and-time';
 
 import { Database } from '@/utils/database';
 import { Difference } from '@/types';
@@ -21,22 +22,23 @@ export class Bot {
 
         const title = event.title;
         const date = new Date(event.start);
-        const dateTxt = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`;
+        const dateTxt = dateAndTime.format(date, 'DD/MM/YYYY HH:mm') ?? date;
         const totSpots = event.participantLimit;
         const availableSpots = event.participantLimit - event.partecipantsRegistered;
         const price = event.price ? `${event.price}€` : '0€';
+        const discounted = event.discountedPrice ? `(discounted to ${event.discountedPrice}€)` : '';
         const link = `https://tumi.esn.world/events/${event.id}`;
 
         switch (type) {
             case 'new':
                 return `
-                There is a new event "${title}", on ${dateTxt} with ${availableSpots}/${totSpots} spots available, with price ${price}
-                The link to the event is ${link}
+                There is a new event <b>"${title}"</b>, on <b>${dateTxt}</b> with <b>${availableSpots}</b>/${totSpots} spots available, with price <b>${price}${discounted}</b>.
+The link to the event is ${link}
                 `;
             case 'set_free':
                 return `
-                The event "${event.title}", on ${dateTxt} is again available, with ${availableSpots}/${totSpots} spots, with price ${price}.
-                The link to the event is ${link}
+                The event <b>"${event.title}"</b>, on <b>${dateTxt}</b> is again available, with <b>${availableSpots}</b>/${totSpots} spots, with price <b>${price}${discounted}</b>.
+The link to the event is ${link}
                 `;
         }
     }
@@ -51,7 +53,7 @@ export class Bot {
         const message = this.getMessageFromDifference(difference);
         const chatIds = await this.database.getChats();
         for (const chatId of chatIds) {
-            await this.bot.telegram.sendMessage(chatId, message);
+            await this.bot.telegram.sendMessage(chatId, message, { parse_mode: 'HTML' });
         }
     }
 
