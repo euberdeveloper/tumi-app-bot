dfd773d8721d2c9fbc0d29f2ad395a77a5f0c655
euberdeveloper <euberdeveloper@gmail.com>
2022-03-03 02:14:34 +0100

    refactor(scripts): add help, stdout bash style and pretty
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- scripts/printEvents.ts
+++ scripts/printEvents.ts
@@ -1,18 +1,22 @@
 import './utils/moduleAlias';
 
-import logger from 'euberlog';
+import { Logger } from 'euberlog';
+import * as minimist from 'minimist';
 
 import { Database } from '@/utils/database';
 import OPTIONS from '@/options';
 
-/**
- * Prints the events of the database.
- */
-(async function () {
+const args = minimist(process.argv.slice(2));
+const logger = new Logger({
+    scope: 'printEvents',
+    debug: args.debug
+});
+
+async function executeTask(indentation: number): Promise<void> {
     let database: Database | null = null;
 
     try {
-        logger.info('Starting...');
+        logger.debug('Starting...');
 
         database = new Database({
             host: OPTIONS.redis.host,
@@ -21,12 +25,34 @@ import OPTIONS from '@/options';
         logger.debug('Database instance created');
 
         const events = await database.getEvents();
-        logger.success('The events are: ', JSON.stringify(events, null, 2));
+        logger.debug('The events are: ');
+
+        console.log(JSON.stringify(events, null, indentation));
     } catch (error: any) {
         logger.error(error);
     } finally {
         if (database !== null) {
             await database.close();
         }
+    }
+}
+
+(async function () {
+    try {
+        if (args.help) {
+            console.log(`
+Usage: npm run scripts:print-events -- [--debug] [--help] [--pretty boolean|number]
+Prints the events of the database as a json in the stdout.
+If the parameter --help is passed, the help is printed.
+If the parameter --debug is passed, there will also be a debug log.
+If the parameter --pretty is passed, the output will be pretty printed (a number specifies the indentation, which is 2 by default).
+            `);
+        } else {
+            const indentation =
+                args.pretty === undefined ? 0 : typeof args.pretty === 'number' ? args.pretty : args.pretty ? 2 : 0;
+            await executeTask(indentation);
+        }
+    } catch (error: any) {
+        logger.error(error);
     }
 })();
--- scripts/removeEvents.ts
+++ scripts/removeEvents.ts
@@ -1,14 +1,17 @@
 import './utils/moduleAlias';
 
-import logger from 'euberlog';
+import { Logger } from 'euberlog';
+import * as minimist from 'minimist';
 
 import { Database } from '@/utils/database';
 import OPTIONS from '@/options';
 
-/**
- * Removes the events from the database.
- */
-(async function () {
+const args = minimist(process.argv.slice(2));
+const logger = new Logger({
+    scope: 'removeEvents'
+});
+
+async function executeTask(): Promise<void> {
     let database: Database | null = null;
 
     try {
@@ -21,12 +24,28 @@ import OPTIONS from '@/options';
         logger.debug('Database instance created');
 
         await database.resetEvents();
-        logger.success('Events reset successfully');
+        logger.success('The events have been removed');
     } catch (error: any) {
         logger.error(error);
     } finally {
         if (database !== null) {
             await database.close();
         }
+    }
+}
+
+(async function () {
+    try {
+        if (args.help) {
+            console.log(`
+Usage: npm run scripts:remove-events -- [--help]
+Removes the events in the database.
+If the parameter --help is passed, the help is printed.
+            `);
+        } else {
+            await executeTask();
+        }
+    } catch (error: any) {
+        logger.error(error);
     }
 })();
