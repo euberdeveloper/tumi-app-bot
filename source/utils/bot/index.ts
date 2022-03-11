2d3eabe43517e357789b71d0e6494fa1878ad845
euberdeveloper <euberdeveloper@gmail.com>
2022-03-12 00:22:54 +0100

    fix(bot): fix display price and number of partecipants
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/utils/bot/index.ts
+++ source/utils/bot/index.ts
@@ -81,8 +81,8 @@ ${commandsText}`;
             (dateAndTime as any).formatTZ(date, 'DD/MM/YYYY HH:mm', 'Europe/Berlin') ??
             `${date.toLocaleString('de')} UTC`;
         const totSpots = event.participantLimit;
-        const availableSpots = event.participantLimit - (event.partecipantsRegistered ?? 0);
-        const price = event.price ? `(${event.price}€)` : '';
+        const availableSpots = event.participantLimit - (event.participantsRegistered ?? 0);
+        const price = event.price ? `(<b>${event.price}€</b>)` : '';
         const link = `https://tumi.esn.world/events/${event.id}`;
 
         switch (type) {
